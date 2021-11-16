faab0626e7f4b29a266a9f366260f3e01b385f83
Xanax <Merwwj@gmail.com>
2021-11-16 12:17:44 +0330

    token security

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const {Client , Intents} = require('discord.js');
 const client = new Client({intents:[Intents.FLAGS.GUILDS]});
-require('dotenv').config();
-//hello this is my bot 
+
 client.on('ready',()=>{
     console.log('Ready!');
 });
