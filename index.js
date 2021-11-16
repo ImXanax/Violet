2821db361f3ac5ef92015850c3bbcadbde25091f
Xanax <Merwwj@gmail.com>
2021-11-16 12:00:43 +0330

    test

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const {Client , Intents} = require('discord.js');
 const client = new Client({intents:[Intents.FLAGS.GUILDS]});
 require('dotenv').config();
-
+//hello this is my bot 
 client.on('ready',()=>{
     console.log('Ready!');
 });
-
 (async () =>{
     client.login(process.env.TOKEN);
 })();
