c3b547a0e5903171fab4dbbac6dd499406c2ef6f
Xanax <Merwwj@gmail.com>
2021-11-16 13:21:50 +0330

    Commands and Events Handler + Ping Command

--- index.js
+++
@@ -1,9 +0,0 @@
-const {Client , Intents} = require('discord.js');
-const client = new Client({intents:[Intents.FLAGS.GUILDS]});
-
-client.on('ready',()=>{
-    console.log('Ready!');
-});
-(async () =>{
-    client.login(process.env.TOKEN);
-})();
