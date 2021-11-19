c174e9ddb1b44c7036b591ebb0cb11578eeb6586
Xanax <Merwwj@gmail.com>
2021-11-19 23:28:02 +0330

    fix ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,7 +6,7 @@ module.exports = {
         const embed = new MessageEmbed()
             .setDescription("\`\`\` IM BACK BITCH \`\`\`")
             .setColor('#03f2ff')
-        client.channels.cache.get('762670306824290321').send({emeds:[embed]});
+        client.channels.cache.get('762670306824290321').send("READY");
         console.log('Ready!');
     },
 };
