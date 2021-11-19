04bd510ce72e6650e30b9c39d7320bce2ef092b1
Xanax <Merwwj@gmail.com>
2021-11-19 23:11:15 +0330

    notify in channel

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,13 @@
 module.exports = {
     name: 'ready',
     once: true,
-    async execute(){
-        const channel = '762670306824290321';
-        channel.send('Ready');
+    async execute(client){
+        client.channels.cache.get('762670306824290321').send({
+        embed: {
+            description: "```All System's Running```",
+            color: "#0a00ff"
+        }
+    })
         console.log('Ready!');
     },
 };
