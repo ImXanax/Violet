ddfb5380ce76a064702118a60f48e924afa0bbc7
Xanax <Merwwj@gmail.com>
2021-11-19 23:13:23 +0330

    TEST ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,12 +2,7 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client){
-        client.channels.cache.get('762670306824290321').send({
-        embed: {
-            description: "```All System's Running```",
-            color: "#0a00ff"
-        }
-    })
+        client.channels.cache.get('762670306824290321').send("READY");
         console.log('Ready!');
     },
 };
