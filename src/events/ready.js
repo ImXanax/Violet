ecb22e3fa28a20f7bc6fc67db5d39a85f3b92898
Xanax <Merwwj@gmail.com>
2021-11-19 23:20:01 +0330

    sends ready with embed when bot online

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,12 @@
+const {MessageEmbed} = require("discord.js");
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client){
-        client.channels.cache.get('762670306824290321').send("READY");
+        const embed = new MessageEmbed()
+            .setDescription("\`\`\` IM BACK BITCH \`\`\`")
+            .setColor('#03f2ff')
+        client.channels.cache.get('762670306824290321').send({emeds:[embed]});
         console.log('Ready!');
     },
 };
