1df26144d4d2a662b161e6f25955edb4ff4c4091
Xanax <Merwwj@gmail.com>
2021-11-22 18:31:03 +0330

    addition cmd

--- src/commands/Information/add.js
+++ src/commands/Information/add.js
@@ -7,18 +7,19 @@ module.exports = {
         .setName('add')
         .setDescription('How long the bot has been operational')
         .addIntegerOption(option => 
-            option.setName("Number")
+            option.setName("num")
                 .setDescription('First Number')
                 .setRequired(true))
         .addIntegerOption(option2 =>
-            option2.setName("Number Two")
+            option2.setName("num2")
                 .setDescription('Second Number')
                 .setRequired(true)),
     async execute(interaction){ 
         const num1 = options.getNumber('num1')
         const num2 = options.getNumber('num2')
         interaction.reply({
-            content: `Sum: ${num1+num2}`
+            content: `Sum: ${num1+num2}`,
+            ephemeral: true,
         })
     }
 }
