3d20c023d132daea9714c4adbcacfda3bde35ba3
Xanax <Merwwj@gmail.com>
2021-11-22 18:26:02 +0330

    addition cmd

---
+++ src/commands/Information/add.js
@@ -0,0 +1,24 @@
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const { execute } = require('../../events/ready');
+const {MessageEmbed} = require('discord.js');
+
+module.exports = {
+    data:new SlashCommandBuilder()
+        .setName('add')
+        .setDescription('How long the bot has been operational')
+        .addIntegerOption(option => 
+            option.setName("Number")
+                .setDescription('First Number')
+                .setRequired(true))
+        .addIntegerOption(option2 =>
+            option2.setName("Number Two")
+                .setDescription('Second Number')
+                .setRequired(true)),
+    async execute(interaction){ 
+        const num1 = options.getNumber('num1')
+        const num2 = options.getNumber('num2')
+        interaction.reply({
+            content: `Sum: ${num1+num2}`
+        })
+    }
+}
