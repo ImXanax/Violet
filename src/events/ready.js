b8cc453c2ef867565af567f28f9a75b6feb10d9d
Xanax <Merwwj@gmail.com>
2021-11-22 19:41:06 +0330

    fixing tings and embed in ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client){
-        const embed = new MessageEmbed().setDescription('Im Online').setColor('#000000')
+        const embed = new MessageEmbed().setDescription(`\`\`\`All System's Operational\`\`\``).setColor('#36057c')
         client.channels.cache.get('762670306824290321').send({embeds:[embed]});
         console.log('✅ The bot is online -l||l-');
     },
