52dee8d0748c783080e3989a854ef4bc4367c675
Xanax <Merwwj@gmail.com>
2021-11-28 16:41:12 +0330

    added Intents

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const {Client , Intents, Collection} = require('discord.js');
 const fs = require('fs');
-const client = new Client({intents:[Intents.FLAGS.GUILDS , Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]});
+const client = new Client({intents:[
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.DIRECT_MESSAGES
+    ]});
 
 client.commands = new Collection();
 
