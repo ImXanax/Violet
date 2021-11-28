32d392c42593808107cddfa0771ccd0d50273778
Xanax <Merwwj@gmail.com>
2021-11-28 15:09:10 +0330

    Message Event

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,15 +4,8 @@ module.exports = {
     name: 'messageCreate',
     on: true,
     async execute(message,client){
-        console.log('fire');
-        let userId = '413755451373518864';
-        console.log('fired');
-        if (message.author === userId){
-            console.log('fireed');
-            if(message.content ==='Hii'){
-                console.log('fireeed');
-                message.channel.send('HELLO');
-            }
+        if (message.channel.type === 'DM') {
+            console.log(message.content)
         }
     },
 };
