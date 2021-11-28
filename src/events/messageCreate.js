bc44b8cca111476f49d1cf918f45a74af336bfe6
Xanax <Merwwj@gmail.com>
2021-11-28 15:48:10 +0330

    random test for msg event and Dm response

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -3,7 +3,13 @@ const {MessageEmbed} = require("discord.js");
 module.exports = {
     name: 'messageCreate',
     on: true,
-    async execute(client){
-      console.log('hi');
+    async execute(client,message){
+        if(message.author.bot)return;
+        if(message.author == 'DM'){
+            let response = new MessageEmbed()
+                .setDescription('Hello Please Dont Use Me To Store Your Things Thanks!')
+                .setColor('#36057c')
+            message.author.send({embed:[response]});
+        }     
     },
 };
