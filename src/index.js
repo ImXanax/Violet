b68501ac10eff07e4c483e9a15a0c5c391b14b7f
Xanax <Merwwj@gmail.com>
2021-11-28 16:50:27 +0330

    added Intents

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const client = new Client({intents:[
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Intents.FLAGS.GUILD_MESSAGE_TYPING,
-        Intents.FLAGS.GUILD_SCHEDULED_EVENTS,
         Intents.FLAGS.DIRECT_MESSAGES,
         Intents.FLAGS.DIRECT_MESSAGE_TYPING,
         Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
