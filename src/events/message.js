a078e5299e2b29999e97e6b5aac66721bf8d4368
Xanax <Merwwj@gmail.com>
2021-11-28 14:41:34 +0330

    Message Event

--- src/events/message.js
+++ src/events/message.js
@@ -3,8 +3,9 @@ const {MessageEmbed} = require("discord.js");
 module.exports = {
     name: 'message',
     once: true,
-    async execute(message){
+    async execute(message,client){
         let userID = "413755451373518864";
+        console.log(message.content);
         if(message.author === userID) {
             if(message.content === 'psst')
             {
