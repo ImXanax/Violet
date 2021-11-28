823d6268cc380a0b47eb517b8ae969c59d6f73c9
Xanax <Merwwj@gmail.com>
2021-11-28 17:12:42 +0330

    adding partial CHANNEL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const {Client , Intents, Collection} = require('discord.js');
 const fs = require('fs');
-const client = new Client({partials: ["CHANNEL"],intents:[
+const client = new Client({partials: ["MESSAGE", "CHANNEL", "REACTION"],
+intents:[
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MEMBERS,
