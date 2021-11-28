01f80490b2a3056a9b74e24039d5f38fbc051001
Xanax <Merwwj@gmail.com>
2021-11-28 18:00:47 +0330

    adding partial CHANNEL

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,11 +4,11 @@ module.exports = {
     name: 'messageCreate',
     on: true,
     async execute(client,message){
-        if(message.content === 'Psst'){
+      if(message.channel.type === 'DM'){
             let response = new MessageEmbed()
                 .setDescription('Hello Please Dont Use Me To Store Your Things Thanks!')
                 .setColor('#36057c')
-            message.author.send({embed:[response]});
-        }     
+             message.author.send({embed:[response]})
+        }
     },
 };
