1d18394fe38640ad2a86f6375d8e3400abedc2c8
Xanax <Merwwj@gmail.com>
2021-11-28 16:47:05 +0330

    added Intents

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -5,7 +5,7 @@ module.exports = {
     on: true,
     async execute(client,message){
         //DM
-        if(message.author == 'DM'){
+        if(message.channel.type == 'DM'){
             let response = new MessageEmbed()
                 .setDescription('Hello Please Dont Use Me To Store Your Things Thanks!')
                 .setColor('#36057c')
