77b220ebf9a1995ea4eef4dd34b0eb878887be8e
Xanax <Merwwj@gmail.com>
2021-11-29 11:07:17 +0330

    deleting the msg even

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,14 +4,6 @@ module.exports = {
     name: 'messageCreate',
     on: true,
     async execute(client,message){
-     if(message.author.bot) return;
-      if(message.channel.type === 'DM'){
-          if(message.content==='hi'){
-            let response = new MessageEmbed()
-                .setDescription('Test')
-                .setColor('#36057c')
-             message.author.send({embed:[response]})
-            }
-        }
+     // do nothing
     },
 };
