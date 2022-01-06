e6063adcff2e140ae1bf612459b3dbf9a36cd73f
Xanax <Merwwj@gmail.com>
2022-01-06 22:04:54 +0330

    quiz update

--- src/commands/Information/quiz.js
+++ src/commands/Information/quiz.js
@@ -17,15 +17,28 @@ module.exports = {
             .setDescription(item.question)
             .setColor("#36057c")
             .setFooter("Respond with typing your answer.")
+        const r = new MessageEmbed()
+            .setDescription(`${collected.first().author} Correct Answer.`)
+            .setColor("#36057c")
+        
         interaction.reply({embeds:[q]},{fetchReply:true})
             .then(()=>{
                 interaction.channel.awaitMessages({filter , max:1,time:30000,errors:['time']})
                 .then(collected=>{
-                    interaction.followUp(`${collected.first().author} got the answer correct`)
+                    // correct answer
+                    interaction.followUp({embeds:[r]})
                 })
                 .catch(collected => {
+                    // time ran out 
                     interaction.followUp(`Looks like nobody got the right answer`);
                 });
             });
     }
 }
+
+/*
+▬ 60s time limit
+▬ 2 attempts
+▬ Answers with ABCD options
+▬ Embed for response
+*/
