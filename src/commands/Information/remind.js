1c3e70291583863912f54f0183121710e25217fe
Xanax <Merwwj@gmail.com>
2022-01-10 18:00:18 +0330

    test

---
+++ src/commands/Information/remind.js
@@ -0,0 +1,42 @@
+const {GuildMember,MessageEmbed, Message} = require('discord.js');
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const { execute } = require('../../events/messageCreate');
+module.exports = {
+    data:new SlashCommandBuilder()
+    .setName('remind')
+    .setDescription('sends DM for no reason')
+    .addSubcommand(subcommand => 
+        subcommand
+            .setName('new')
+            .setDescription('Create a new reminder')
+        )
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('old')
+            .setDescription("describes")    
+            ),
+    async execute(ctx){
+            if(ctx.opt.getSubCommand() === 'new'){
+                ctx.reply('new')
+            }
+            else if(ctx.opt.getSubCommand() === 'old'){
+                ctx.reply('old')
+            }
+            else{
+                ctx.reply('no subcommand was used')
+            }
+
+        /*try{
+            const dm = await ctx.member.createDM()
+            await dm.send({content:"hello"})
+            .then(()=> {
+                ctx.reply({content:"Message Sent!"})
+                console.log(ctx);
+            }).catch(()=>{
+                ctx.reply({content:"There was an issue sending the message"});
+            })
+        }catch(e){
+            console.error(e);
+        }*/
+    }
+}
