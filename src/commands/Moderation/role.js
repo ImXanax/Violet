1a9962908517daebdf7000ffddf5071ad9662483
Xanax <Merwwj@gmail.com>
2022-08-14 01:00:36 +0430

    fix embed

--- src/commands/Moderation/role.js
+++ src/commands/Moderation/role.js
@@ -27,52 +27,13 @@ module.exports = {
     const roleEmbed1 = new MessageEmbed()
       .setDescription(
         `
-        :Raven: **Wither**
-        #000000
-        :Lightning: **Golem**
-        #dadada
-        :Apocalypse: **Overworld**
-        #01e3ae
-        :Moonlight: **Elytra**
-        #b1bdf0
-        :Waterfall: **Diamond**
-        #5e80Fb
-        :Butterfly: **River**
-        #0044FF
-        :Vanilla: **Desert**
-        #ffc0a0
-        :Clouds: **Badlands**
-        #ff9393
-        :Zephyr: **Tulip**
-        #ed9fd7        
+        **__COLORS:__**
+        <:Raven:866775351622762526> **Wither ** #000000\n <:Lightning:866775421340352524> **Golem** #dadada\n <:Apocalypse:866787694239154266> **Overworld** #01e3ae\n <:Moonlight:866775523195355207> **Elytra** #b1bdf0\n <:Waterfall:866775592712011787> **Diamond** #5e80Fb\n <:Butterfly:866775676392570880> **River** #0044FF\n <:Vanilla:866775941475467284> **Desert** #ffc0a0\n <:Clouds:866776007980875786> **Badlands** #ff9393\n <:Zephyr:866776168203550780> **Tulip** #ed9fd7\n        
         `
       )
       .setColor("#36057c");
     const roleEmbed2 = new MessageEmbed().setDescription(`
-        :Riot: **Shulker**
-        #bf99e0
-        :Euphoria: **Portal**
-        #Df5bdf
-        :Cyberpunk: **Enderman**
-        #cb0ab8
-        :Violet: **Void**
-        #8e51e9
-        :Amethyst: **Endermite**
-        #7313c8
-        :Olive_Branch: **Turtle**
-        #9dd891
-        :Serpent_Green: **Kelp**
-        #4e812c
-        :Sunlight: **Sand**
-        #ffe58c
-        :Torch: **Blaze**
-        #ffd816
-        :Jumpsuit: **Lava**
-        #ff812d
-        :Chrysalis: **Nether**
-        #ed3030
-        :Neon_Red: **Magma**
-        #b70f0f
+    <:Riot:866776322559049758> **Shulker** #bf99e0\n <:Euphoria:866776409292406844> **Portal** #Df5bdf\n <:Cyberpunk:866776491266146304> **Enderman** #cb0ab8\n <:Violet:866776625923883068> **Void** #8e51e9\n <:Amethyst:866777697798979623> **Endermite** #7313c8\n <:Olive_Branch:866778299384856596> **Turtle** #9dd891\n <:Serpent_Green:866778478724775976> **Kelp** #4e812c\n <:Sunlight:866778568835334194> **Sand** #ffe58c\n <:Torch:866778612186611722> **Blaze** #ffd816\n <:Jumpsuit:866778672409214986> **Lava** #ff812d\n <:Chrysalis:866778728326627328> **Nether** #ed3030\n <:Neon_Red:866778777727926292> **Magma** #b70f0f\n
     `).setColor("#36057c")
 
     chl.send({ embeds: [roleEmbed1, roleEmbed2] }).then(() => {
