344025f0e98512b7587727fa3a3b5567e90516df
Xanax <Merwwj@gmail.com>
2022-09-06 18:16:15 +0430

    implemented score / edit panel

--- src/commands/Moderation/edit.js
+++ src/commands/Moderation/edit.js
@@ -59,34 +59,83 @@ module.exports = {
     if (!isAdmin) {
       return ctx.reply("You lack the permission to use this command.");
     }
-
+    let resultEmbed = new MessageEmbed().setColor("#36057c");
     //XP
     if (ctx.options.getSubcommand() === "xp") {
-      console.log("xp");
+      console.log("-XP");
       //INPUTS
       const user = ctx.options.getUser("user");
       let option = ctx.options.getString("option");
+      option = option.toLowerCase();
       const amount = ctx.options.getInteger("amount");
 
       //OPT
       if (option === "add") {
         try {
-          ctx.member.fetch(user.id).then((u) => {
-            Levels.fetch(u.user.id, u.guild.id)
-              .then(() => {
-                Levels.addXp(u.user.id, u.guild.id, amount)
-                  .then(() =>
-                    console.log(`added ${amount} XP to ${u.user.tag}`)
-                  )
-                  .catch((e) => console.error(`err in adding xp: ${e}`));
-              })
-              .catch((e) => console.error(`err in fetching membe: ${e}`));
-          }).catch(e=>console.error(e))
+          ctx.member
+            .fetch(user.id)
+            .then((u) => {
+              Levels.fetch(u.user.id, u.guild.id)
+                .then(() => {
+                  Levels.addXp(u.user.id, u.guild.id, amount)
+                    .then(() => {
+                      resultEmbed.setDescription(
+                        `Added **${amount}** of XP to **${u.user.tag}**`
+                      );
+                      ctx.reply({ embeds: [resultEmbed] });
+                    })
+                    .catch((e) => console.error(`err in adding xp: ${e}`));
+                })
+                .catch((e) => console.error(`err in fetching member: ${e}`));
+            })
+            .catch((e) => console.error(e));
         } catch (e) {
           console.error(`err in everything?: ${e}`);
         }
       } else if (option === "set") {
+        try {
+          ctx.member
+            .fetch(user.id)
+            .then((u) => {
+              Levels.fetch(u.user.id, u.guild.id)
+                .then(() => {
+                  Levels.setXp(u.user.id, u.guild.id, amount)
+                    .then(() => {
+                      resultEmbed.setDescription(
+                        `Set **${amount}** of XP for **${u.user.tag}**`
+                      );
+                      ctx.reply({ embeds: [resultEmbed] });
+                    })
+                    .catch((e) => console.error(`err in setting xp: ${e}`));
+                })
+                .catch((e) => console.error(`err in fetching member: ${e}`));
+            })
+            .catch((e) => console.error(e));
+        } catch (e) {
+          console.error(`err in everything?: ${e}`);
+        }
       } else if (option === "remove") {
+        try {
+          ctx.member
+            .fetch(user.id)
+            .then((u) => {
+              Levels.fetch(u.user.id, u.guild.id)
+                .then(() => {
+                  Levels.subXp(u.user.id, u.guild.id, amount)
+                    .then(() => {
+                      resultEmbed.setDescription(
+                        `Removed **${amount}** of XP from **${u.user.tag}**`
+                      );
+                      ctx.reply({ embeds: [resultEmbed] });
+                    })
+                    .catch((e) => console.error(`err in removing xp: ${e}`));
+                })
+                .catch((e) => console.error(`err in fetching member: ${e}`));
+            })
+            .catch((e) => console.error(e));
+        } catch (e) {
+          console.error(`err in everything?: ${e}`);
+        }
       } else {
         ctx.reply({
           content: `provided argument is wrong\navailable options are: **[add/set/remove]**`,
@@ -107,14 +156,87 @@ module.exports = {
 
     //LEVEL
     else if (ctx.options.getSubcommand() === "level") {
-      console.log("lvl");
+      console.log("-LVL");
       //INPUTS
       const user = ctx.options.getUser("user");
-      const option = ctx.options.getString("option");
+      let option = ctx.options.getString("option");
+      option = option.toLowerCase();
       const amount = ctx.options.getInteger("amount");
+
+      if (option === "add") {
+        try {
+          ctx.member
+            .fetch(user.id)
+            .then((u) => {
+              Levels.fetch(u.user.id, u.guild.id)
+                .then(() => {
+                  Levels.addLevel(u.user.id, u.guild.id, amount)
+                    .then(() => {
+                      resultEmbed.setDescription(
+                        `Added **${amount}** of LEVEL/S to **${u.user.tag}**`
+                      );
+                      ctx.reply({ embeds: [resultEmbed] });
+                    })
+                    .catch((e) => console.error(`err in adding level: ${e}`));
+                })
+                .catch((e) => console.error(`err in fetching member: ${e}`));
+            })
+            .catch((e) => console.error(e));
+        } catch (e) {
+          console.error(`err in everything?: ${e}`);
+        }
+      } else if (option === "set") {
+        try {
+          ctx.member
+            .fetch(user.id)
+            .then((u) => {
+              Levels.fetch(u.user.id, u.guild.id)
+                .then(() => {
+                  Levels.setLevel(u.user.id, u.guild.id, amount)
+                    .then(() => {
+                      resultEmbed.setDescription(
+                        `Set **${amount}** of LEVEL/S for **${u.user.tag}**`
+                      );
+                      ctx.reply({ embeds: [resultEmbed] });
+                    })
+                    .catch((e) => console.error(`err in setting LEVEL: ${e}`));
+                })
+                .catch((e) => console.error(`err in fetching member: ${e}`));
+            })
+            .catch((e) => console.error(e));
+        } catch (e) {
+          console.error(`err in everything?: ${e}`);
+        }
+      } else if (option === "remove") {
+        try {
+          ctx.member
+            .fetch(user.id)
+            .then((u) => {
+              Levels.fetch(u.user.id, u.guild.id)
+                .then(() => {
+                  Levels.subXp(u.user.id, u.guild.id, amount)
+                    .then(() => {
+                      resultEmbed.setDescription(
+                        `Removed **${amount}** of LEVEL/S from **${u.user.tag}**`
+                      );
+                      ctx.reply({ embeds: [resultEmbed] });
+                    })
+                    .catch((e) => console.error(`err in removing LEVEL: ${e}`));
+                })
+                .catch((e) => console.error(`err in fetching member: ${e}`));
+            })
+            .catch((e) => console.error(e));
+        } catch (e) {
+          console.error(`err in everything?: ${e}`);
+        }
+      } else {
+        ctx.reply({
+          content: `provided argument is wrong\navailable options are: **[add/set/remove]**`,
+        });
+      }
     } else {
       ctx.reply({
-        content: `this got triggered for some reason`,
+        content: `provided argument is wrong\navailable options are: **[xp/level]**`,
       });
     }
   },
