e6698457ff52d28335765f14ae680fb64e9d83e1
Xanax <Merwwj@gmail.com>
2024-03-18 23:38:16 +0330

    Implement Dm.ts with EmbedBuilder

--- src/commands/Staff/Dm.ts
+++ src/commands/Staff/Dm.ts
@@ -25,9 +25,40 @@ export default new Command({
   async run(interaction: ChatInputCommandInteraction): Promise<void> {
     const user = interaction.options.getUser("user");
     const message = interaction.options.getString("message") ?? "Nothing";
+    try {
+      const dmEmbed = new EmbedBuilder()
+        .setDescription(message)
+        .setColor(X.hex.primary);
+      const resEmbed = new EmbedBuilder()
+        .addFields(
+          {
+            name: "Message:",
+            value: message,
+          },
+          {
+            name: "User ID:",
+            value: `${user?.id}`,
+          },
+          {
+            name: "Username:",
+            value: `${user?.username}`,
+          }
+        )
+        .setFooter({
+          text: `Sent At: ${interaction.createdAt}`,
+          iconURL:
+            user?.avatarURL({ forceStatic: true, size: 32 }) ?? undefined,
+        })
+        .setColor(X.hex.primary);
 
-    const dm = await user?.createDM();
-    dm?.send(message);
-    interaction.reply("Sent");
+      const dm = await user?.createDM();
+      dm?.send({ embeds: [dmEmbed] }).then(() => {
+        interaction.reply({ embeds: [resEmbed] });
+      });
+    } catch (err) {
+      interaction.reply("Failed");
+
+      console.log(err);
+    }
   },
 });
