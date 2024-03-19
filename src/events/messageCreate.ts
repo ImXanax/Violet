addc827c7d070aabd581afdd20946ddcf0a5212f
Xanax <Merwwj@gmail.com>
2024-03-19 22:41:43 +0330

    Adding messageCreate event listener

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,29 @@
+import { ChannelType, Client, EmbedBuilder, Events, Message } from "discord.js";
+import axios from "axios";
+import * as cheerio from "cheerio";
+import * as fs from "fs";
+import chalk from "chalk";
+import { X } from "../funcs/Xan.js";
+
+export default {
+  name: Events.MessageCreate,
+  async run(message: Message, client: Client) {
+    if (message.author.bot) return;
+    if (message.channel.type === ChannelType.DM) {
+      console.log(message);
+      const dmLogEmbed = new EmbedBuilder()
+        .addFields(
+          { name: "Content:", value: `${message.content}` },
+          { name: "ID:", value: `${message.author.id}` }
+        )
+        .setAuthor({
+          name: `${message.author.username}`,
+          iconURL: message.author.avatarURL() ?? undefined,
+        })
+        .setFooter({ text: `Created At: ${message.createdAt}` });
+        await client.channels.fetch("1012390735611433031").then((ch) => {
+        if (ch?.isTextBased()) ch.send({ embeds: [dmLogEmbed] });
+      });
+    }
+  },
+};
